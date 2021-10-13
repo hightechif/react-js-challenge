aa27c94b96cc1fb5b8eaf610e3fab71de52e880f
R. Fadhil <ridhanfadhilah@gmail.com>
2021-10-13 21:53:02 +0700

    challenge 2 - section 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Router from './router';
+import './App.css';
 import './styles/GlobalStyle.css';
+import './styles/Home.css';
 import './styles/Button.css';
 import './styles/ButtonNotif.css';
 import './styles/RatingRound.css';
 import './styles/Badge.css';
 import './styles/BadgeCloud.css';
 import './styles/Modal.css';
+import './styles/Challenge02.css'
+import './styles/Section1.css';
+import './styles/Cards.css';
 
 function App() {
   return <Router />;
