641e3bb754b43646c363c3dee8e6737542cac172
R. Fadhil <ridhanfadhilah@gmail.com>
2021-10-14 17:37:30 +0700

    challenge 2 - section 4

--- src/components/Section/Section4.js
+++ src/components/Section/Section4.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
-const Section4 = () => {
+const Section4 = (props) => {
     return (
-        <section className="section-3">
-            
+        <section className="section-4">
+            <div className="section-4__wrapper">
+                <div className="banner">
+                    <figure className='box__item__pic-wrap'>
+                        <img 
+                            className='box__item__img'
+                            src="img/Media3.png" 
+                            alt="Media" 
+                        />
+                    </figure>
+                    <div className="description">
+                        <p>Author name</p>
+                        <h1>The best way to wireframe a website</h1>
+                        <Link 
+                            className='hyperlink'
+                            to='#'
+                            target=''
+                        >
+                            Read Mode
+                        </Link>
+                    </div>
+                </div>
+                <article className='article' id="article1">
+                    <h3>Keys to writing copy that actually converts and sells users</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore...</p>
+                    <Link
+                        className='hyperlink'
+                        to='#'
+                        target=''
+                    >
+                        Read More
+                    </Link>
+                </article>
+                <article className='article' id="article2">
+                    <h3>Keys to writing copy that actually converts and sells users</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore...</p>
+                    <Link
+                        className='hyperlink'
+                        to='#'
+                        target=''
+                    >
+                        Read More
+                    </Link>
+                </article>
+            </div>
         </section>
     )
 }
