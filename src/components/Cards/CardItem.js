a26a7d3dba5110253dd1b9f0fb6a366bd49dd968
R. Fadhil <ridhanfadhilah@gmail.com>
2021-10-28 13:34:43 +0700

    refactor to implement styled components

--- src/components/Cards/CardItem.js
+++ src/components/Cards/CardItem.js
@@ -1,28 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-
-const CardItemStyle = styled.li`
-	width: 270px;
-`;
+import { StyledCard } from './Card.styled';
 
 function CardItem(props) {
 	return (
-		<>
-			<CardItemStyle>
-				<figure className='cards__item__pic-wrap'>
-					<img
-						className='cards__item__img'
-						alt='Media'
-						src={props.src}
-					/>
-				</figure>
-				<div className='cards__item__info'>
-					<h3 className='cards__item__text'>{props.text}</h3>
-					<Link className='cards__item__link' to={props.path} target=''>See More {'>'}</Link>
-				</div>
-			</CardItemStyle>
-		</>
+		<StyledCard>
+			<figure className='cards__item__pic-wrap'>
+				<img
+					className='cards__item__img'
+					alt='Media'
+					src={props.src}
+				/>
+			</figure>
+			<div className='cards__item__info'>
+				<h3 className='cards__item__text'>{props.text}</h3>
+				<Link className='cards__item__link' to={props.path} target=''>See More {'>'}</Link>
+			</div>
+		</StyledCard>
 	)
 }
 
