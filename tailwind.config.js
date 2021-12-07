e3b8912d8a446566f025078a343fb2982c8906f7
R. Fadhil <ridhanfadhilah@gmail.com>
2021-12-07 16:05:05 +0700

    Fix(react-js): Fix Tailwind depedency for production

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require('tailwindcss'),
+    require('tailwindcss@npm:@tailwindcss/postcss7-compat'),
     require('autoprefixer')
   ],
 }
